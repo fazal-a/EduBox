4ee8b6ad37103b1104a3ff94f5103edad7b6cb98
fazal-a <thefazalabbas@gmail.com>
2023-06-02 00:34:24 +0500

    homepage updated

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -4,12 +4,9 @@ const Footer = () => {
   return (
     <footer className="footer mt-5">
       <p className="text-center">
-        Developed by <strong><a href="https://github.com/Joseacasado" target="_blank" rel="noreferrer"> Jose A. Casado </a></strong>
-        and <strong><a href="https://github.com/marisa2306" target="_blank" rel="noreferrer">Marisa Vitale </a></strong>
-        within <strong> Ironack </strong><em> Web Development Bootcamp</em>
+        Developed by <strong><a href="https://github.com/fazal-a" target="_blank" rel="noreferrer"> Fazal Abbas </a></strong>
       </p>
     </footer>
   )
 }
-
 export default Footer
